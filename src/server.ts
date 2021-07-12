5ff60091221bdd0ff133f978d4f5c164c57b9fbb
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-07-12 18:07:59 -0300

    Tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import swaggerUI from 'swagger-ui-express'
 import 'reflect-metadata'
 
+import { AppError } from './errors/AppError'
 import { router } from './routes'
 import swaggerFile from './swagger.json'
-
 import './database'
 import './shared/container'
 
@@ -15,6 +16,19 @@ app.use(express.json())
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 app.use(router)
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${error.message}`,
+  })
+})
 
 app.listen(3333, () => {
   console.log('Server is running')
