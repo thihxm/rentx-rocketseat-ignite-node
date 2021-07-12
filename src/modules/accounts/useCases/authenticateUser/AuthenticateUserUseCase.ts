8729859a56735dd5dc6e6c292885f86d00103b98
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-07-12 17:45:21 -0300

    Rota de autenticação do usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+interface IReponse {
+  user: {
+    name: string
+    email: string
+  }
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IReponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or password incorrect')
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect')
+    }
+
+    const token = sign({}, 'supertokenseguro', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
+    const tokenReturn: IReponse = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    }
+
+    return tokenReturn
+  }
+}
+export { AuthenticateUserUseCase }
