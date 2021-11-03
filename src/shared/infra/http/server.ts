745bf785abfa9eadf23328d0442f5dc96f5a724f
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-11-03 19:11:36 -0300

    Supertest + api endpoint testing

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express'
-import 'express-async-errors'
-import swaggerUI from 'swagger-ui-express'
-import 'reflect-metadata'
-
-import { AppError } from '@shared/errors/AppError'
-import { router } from '@shared/infra/http/routes'
-import createConnection from '@shared/infra/typeorm'
-
-import swaggerFile from '../../../swagger.json'
-import '@shared/container'
-
-createConnection()
-const app = express()
-
-app.use(express.json())
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
-
-app.use(router)
-
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      message: error.message,
-    })
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${error.message}`,
-  })
-})
+import { app } from './app'
 
 app.listen(3333, () => {
   console.log('Server is running')
